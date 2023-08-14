7f373a237173aaeade38d9d9b5c5b748ddfd44da
Raphael Rodrigues <noh4nsen@gmail.com>
2023-08-14 16:32:22 -0300

    chore: debug script

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,9 @@ report="[]"
 echo $projects | jq -r '.projects[]' | while read -r project; do
     cd $GITHUB_WORKSPACE/$project;
     json_object=$(jq -n -c --argjson $project "$(tflint --format=json)" '$ARGS.named')
+    echo $json_object
     report=$(jq --argjson obj "$json_object" '. + [$obj]' <<< "$report")
+    echo $report
 done
 
 echo $report | jq
