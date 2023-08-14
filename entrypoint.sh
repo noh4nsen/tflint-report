614c52e62539f80ee399742c7895968fc5cc7586
Raphael Rodrigues <noh4nsen@gmail.com>
2023-08-14 15:31:25 -0300

    fix: GITHUB_WORKSPACE variable name

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,6 @@ set -euo pipefail
 projects=$(echo $1 | tr -d "'")
 
 echo $projects | jq -r '.projects[]' | while read -r project; do
-    echo $GITHUB_WORSKPACE/$project;
+    echo $GITHUB_WORKSPACE/$project;
     ls -la;
 done
