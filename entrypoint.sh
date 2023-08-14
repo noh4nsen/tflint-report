084c6f32e6cd557d9bcabfa2af1d8e21375a50f6
Raphael Rodrigues <noh4nsen@gmail.com>
2023-08-14 15:29:11 -0300

    feat: change project inside for loop

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 
 projects=$(echo $1 | tr -d "'")
 
-echo $projects | jq -r '.projects[]' | while read -r repo; do
-    echo $repo;
+echo $projects | jq -r '.projects[]' | while read -r project; do
+    echo $GITHUB_WORSKPACE/$project;
+    ls -la;
 done
