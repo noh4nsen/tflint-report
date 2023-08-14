afe192b45ff903ea8edc8c0ac5588290b2e21f66
Raphael Rodrigues <noh4nsen@gmail.com>
2023-08-14 15:37:30 -0300

    fix: jq argjson parameters

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,6 @@ projects=$(echo $1 | tr -d "'")
 
 echo $projects | jq -r '.projects[]' | while read -r project; do
     cd $GITHUB_WORKSPACE/$project;
-    report=$(jq -n -c --argjson $project "$(tflint)" '$ARGS.named')
+    report=$(jq -n -c --argjson $(echo $project) "$(tflint)" '$ARGS.named')
     echo $report
 done
