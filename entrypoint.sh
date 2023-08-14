7c629e64dfcc959cfb58e79cd17d2f025ea3b54a
Raphael Rodrigues <noh4nsen@gmail.com>
2023-08-14 17:35:17 -0300

    feat: better logging and no subshell spawning on entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,15 +4,16 @@ set -euo pipefail
 
 projects=$(echo $1 | tr -d "'")
 report="[]"
-echo $report
 
-echo $projects | jq -r '.projects[]' | while read -r project; do
+while read -r project; do
+    echo "--- Running TFLint on $project ---"
+
     cd $GITHUB_WORKSPACE/$project
     json_object=$(jq -n -c --argjson $project "$(tflint --format=json)" '$ARGS.named')
+    echo $json_object | jq --arg project $project '.[$project].issues[].rule.name'
     report=$(jq --argjson obj "$json_object" '. + [$obj]' <<< "$report")
-    echo $report | jq
-done
 
-echo $report
+    echo -e "--- Finished Report on $project ---\n"
+done < <(echo $projects | jq -r '.projects[]' )
 
 echo "tflint_report='$(echo $report)'" >> $GITHUB_OUTPUT
