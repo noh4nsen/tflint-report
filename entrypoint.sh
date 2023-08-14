bdf63479f3375d5d403f9dc2c839369c04321ca4
Raphael Rodrigues <noh4nsen@gmail.com>
2023-08-14 16:30:41 -0300

    fix: report array output

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,12 @@
 set -euo pipefail
 
 projects=$(echo $1 | tr -d "'")
+report="[]"
 
 echo $projects | jq -r '.projects[]' | while read -r project; do
     cd $GITHUB_WORKSPACE/$project;
-    report=$(jq -n -c --argjson $(echo $project) "$(tflint)" '$ARGS.named')
-    echo $report
+    json_object=$(jq -n -c --argjson $($project) "$(tflint --format=json)" '$ARGS.named')
+    report=$(jq --argjson obj "$json_object" '. + [$obj]' <<< "$report")
 done
+
+echo $report | jq
