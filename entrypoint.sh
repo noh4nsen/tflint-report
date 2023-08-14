f60deba0b3162c1d0ccb54527ca3be7ef70cbaa6
Raphael Rodrigues <noh4nsen@gmail.com>
2023-08-14 15:36:39 -0300

    feat: output tflint report into variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,6 @@ projects=$(echo $1 | tr -d "'")
 
 echo $projects | jq -r '.projects[]' | while read -r project; do
     cd $GITHUB_WORKSPACE/$project;
-    ls -la;
+    report=$(jq -n -c --argjson $project "$(tflint)" '$ARGS.named')
+    echo $report
 done
