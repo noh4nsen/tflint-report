3a692d4067709541f0c86f6dbab175f983b92768
Raphael Rodrigues <noh4nsen@gmail.com>
2023-08-14 16:46:20 -0300

    debug script

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,14 +4,15 @@ set -euo pipefail
 
 projects=$(echo $1 | tr -d "'")
 report="[]"
+echo $report
 
 echo $projects | jq -r '.projects[]' | while read -r project; do
-    cd $GITHUB_WORKSPACE/$project;
-    json_object=$(jq -n -c --argjson $project "$(tflint --format=json)" '$ARGS.named');
-    report=$(jq --argjson obj "$json_object" '. + [$obj]' <<< "$report");
+    cd $GITHUB_WORKSPACE/$project
+    json_object=$(jq -n -c --argjson $project "$(tflint --format=json)" '$ARGS.named')
+    report=$(jq --argjson obj "$json_object" '. + [$obj]' <<< "$report")
     echo $report
 done
 
-echo "$report"
+echo $report
 
 echo "tflint_report='$(echo $report)'" >> $GITHUB_OUTPUT
