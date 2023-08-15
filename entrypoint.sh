dd1df39a621f79c59f19595925e326ab91f326af
Raphael Rodrigues <noh4nsen@gmail.com>
2023-08-15 13:42:14 -0300

    feat: check if projects variable is empty

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,9 @@ set -euo pipefail
 projects=$1
 report="[]"
 
-if [[ $(echo $projects | tr -d "'" | jq -r '.projects[]') == "null" ]]; then
-    echo "--- No projects to scan ---"
+#check $projects contains null word in it without "" or '', if yes then exit the action without error
+if [[ $projects == *null* ]]; then
+    echo "--- No projects to run TFLint on ---"
     echo "tflint_report='$(echo $report)'" >> $GITHUB_OUTPUT
     exit 0
 fi
