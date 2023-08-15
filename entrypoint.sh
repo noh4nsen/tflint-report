d475f1b676b8da77616ef408c47541db691eb0fb
Raphael Rodrigues <noh4nsen@gmail.com>
2023-08-15 11:55:12 -0300

    feat: check if projects variable is null

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,12 @@ set -euo pipefail
 projects=$1
 report="[]"
 
+if [[ $(echo $projects | tr -d "'" | jq -r '.projects[]') == "null" ]]; then
+    echo "--- No projects to scan ---"
+    echo "tflint_report='$(echo $report)'" >> $GITHUB_OUTPUT
+    exit 0
+fi
+
 while read -r project; do
     echo "--- Running TFLint on $project ---"
 
