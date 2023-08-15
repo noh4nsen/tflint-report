c2a98080a76e1ee60163ee80db4acc3c72f3cf79
Raphael Rodrigues <noh4nsen@gmail.com>
2023-08-15 14:01:25 -0300

    changes on output

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 
 projects=$1
 report="[]"
+report_full=""
 
 #check $projects contains null word in it without "" or '', if yes then exit the action without error
 if [[ $projects == *null* ]]; then
@@ -17,8 +18,11 @@ while read -r project; do
 
     cd $GITHUB_WORKSPACE/$project
     report=$(jq --argjson obj "$(jq -n -c --argjson $project "$(tflint --format=json)" '$ARGS.named')" '. + [$obj]' <<< "$report")
+    report_full=$(echo "--- Project $project ---" && "$(tflint)" <<< "$report_full")
 
     echo -e "--- Finished Report on $project ---\n"
 done < <(echo $projects | tr -d "'" | jq -r '.projects[]' )
+
+echo $report_full
 
 echo "tflint_report='$(echo $report)'" >> $GITHUB_OUTPUT
