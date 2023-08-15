58f27de15127dbb377f236ecf0ffef43496f9af1
Raphael Rodrigues <noh4nsen@gmail.com>
2023-08-15 15:33:00 -0300

    finish entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,7 @@ set -euo pipefail
 
 projects=$1
 report="[]"
-report_full=""
 
-#check $projects contains null word in it without "" or '', if yes then exit the action without error
 if [[ $projects == *null* ]]; then
     echo "--- No projects to run TFLint on ---"
     echo "tflint_report='$(echo $report)'" >> $GITHUB_OUTPUT
@@ -18,11 +16,9 @@ while read -r project; do
 
     cd $GITHUB_WORKSPACE/$project
     report=$(jq --argjson obj "$(jq -n -c --argjson $project "$(tflint --format=json)" '$ARGS.named')" '. + [$obj]' <<< "$report")
-    report_full=$(echo "--- Project $project ---" && "$(tflint)" <<< "$report_full")
 
     echo -e "--- Finished Report on $project ---\n"
 done < <(echo $projects | tr -d "'" | jq -r '.projects[]' )
 
-echo $report_full
 
 echo "tflint_report='$(echo $report)'" >> $GITHUB_OUTPUT
