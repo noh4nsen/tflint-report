a29b2d3dbfd24c7d28227a9cd04418b2468f1574
Raphael Rodrigues <noh4nsen@gmail.com>
2023-08-23 12:32:13 -0300

    debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,4 +23,4 @@ done < <(echo $projects | tr -d "'" | jq -r '.projects[]' )
 encoded_report=$(echo -n $report | base64)
 echo -n $encoded_report
 
-echo -n "tflint_report=$encoded_report" >> $GITHUB_OUTPUT
+echo -n "tflint_report=$(echo -n $encoded_report)" >> $GITHUB_OUTPUT
